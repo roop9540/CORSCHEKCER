d94a49d84009b5d4d500038b032e4f4827e3c6cd
Roop <roop9540@gmail.com>
2023-04-13 01:26:13 +0530

    farzi

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { USER } = require('./userModel');
 
 const PORT = process.env.PORT || 2100;
 const MONGO_URL = process.env.MONGO_URL;
-
 const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
@@ -14,12 +13,10 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x - client - key, x - client - token, x - client - secret, Authorization");
-      next();
+    next();
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
-
 app.get('/', (req, res) => {
     res.status(200).send("Server is running")
 })
@@ -45,7 +42,11 @@ app.post('/register', async (req, res) => {
     }
 })
 
-
+app.get('/check', (req, res)=>{
+    let {name, message} = req.query;
+    console.log({name, message});
+    res.status(200).json({name, message});
+})
 mongoose.connect(MONGO_URL).then((e) => {
     console.log("connect to mongodb")
 }).catch((err) => {
